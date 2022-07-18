ea64ff403907b540eee5c21281addbb592267e40
kirillstrizenko <krealstrzh@icloud.com>
2022-07-18 13:23:27 +0300

    Homework #3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int i = 0;
+        while (i < 10) {
+            i ++;
+            System.out.print(i + " ");
+        }
+        System.out.println( );
+        i++;
+        while (i > 1) {
+            i--;
+            System.out.print(i + " ");
+        }
+        System.out.println( );
+        // Задание 2
+        for (int friday = 3; friday <= 31; friday = friday + 7) {
+            System.out.println("Сегодня -- пятница, " + friday + "-е число. Необходимо подготовить отчет!");
+        }
+        // Задание 3
+        int year = 2022;
+        int pastYearPeriod = 200;
+        int pastYear = year - pastYearPeriod;
+        int futureYearPeriod = 100;
+        int futureYear = year + futureYearPeriod;
+        int cometPeriod = 79;
+        for (int cometFlyYear = 0; cometFlyYear <= futureYear; cometFlyYear = cometFlyYear + cometPeriod) {
+            if (cometFlyYear >= pastYear && cometFlyYear <= futureYear) {
+                System.out.println("Год пролета кометы -- " + cometFlyYear);
+            }
+        }
     }
 }
